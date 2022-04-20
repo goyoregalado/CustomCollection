f0189a1aa99202d7dcd90157ce5e7cfc130e23ff
Goyo Regalado <goyoregalado@gmail.com>
2022-04-20 13:51:25 +0100

    Feature: Fully functional CustomColection class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+        String userInput = null;
+
+        CustomColection colection = new CustomColection(3);
+
+        while (!salir) {
+            System.out.println("Introduzca un nombre o pulse Q para salir");
+            userInput = sc.nextLine();
+
+            if (userInput.equals("Q")) {
+                salir = true;
+            } else {
+                colection.insert(userInput, 0);
+            }
+
+            String aux;
+
+            for (int i = 0; i < colection.length(); i++) {
+                aux = (String) colection.getElement(i);
+                System.out.println(i + " - " + aux);
+
+            }
+        }
+
+
     }
 }
